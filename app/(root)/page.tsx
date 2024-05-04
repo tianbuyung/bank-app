e7e0b0326995acab8da69051e591b05183b34e54
Septian Maulana <septiandev028@gmail.com>
2024-05-04 19:21:03 +0700

    feat: create header box as custom components
    feat: integrate into dashboard page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,24 @@
+import HeaderBox from "@/components/customs/HeaderBox";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const loggedIn = {
+    firstName: "Septian",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
