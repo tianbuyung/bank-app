aeb1bd6b2b98b7f85a005392134b8f608d80750a
Septian Maulana <septiandev028@gmail.com>
2024-05-06 19:32:55 +0700

    feat: create sidebar as custom components
    feat: integrate into dashboard page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from "@/components/customs/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "Septian",
+    lastName: "Maulana",
+  };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
