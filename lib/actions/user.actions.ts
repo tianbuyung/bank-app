a553873de1de0483f5829faa684ad8b032942df4
Septian Maulana <septiandev028@gmail.com>
2024-05-12 14:57:38 +0700

    feat: implement sign up logic via integrate appwrite function into sign up user actions
    feat: create get logged in user function in user actions
    
    chore: update city validation in auth form schema utils

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { cookies } from "next/headers";
+
+import { createAdminClient, createSessionClient } from "../server/appwrite";
+import { parseStringify } from "../utils";
+
 export const signIn = async ({ email, password }: signInProps) => {
   try {
     // Mutation / Database / Fetch
@@ -9,10 +15,40 @@ export const signIn = async ({ email, password }: signInProps) => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+
   try {
     // Mutation / Database / Fetch
     // Create a new user account
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error in sign up", error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
